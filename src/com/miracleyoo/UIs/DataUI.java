7da9ff8d4cd11ad427e73e99a8d085ef75d8a0ce
miracleyoo <miracleyoo@163.com>
2019-10-24 01:32:13 -0400

    Add statistics and Data Table

--- src/com/miracleyoo/UIs/DataUI.java
+++ src/com/miracleyoo/UIs/DataUI.java
@@ -2,9 +2,16 @@ package com.miracleyoo.UIs;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.miracleyoo.utils.*;
 
 public class DataUI {
@@ -12,46 +19,216 @@ public class DataUI {
     private JButton ExecuteOneStepBtn;
     private JButton ExecuteMultipleStepBtn;
     private JTable OperandTable;
-    private final int[] slice = {0,5};
+    private JTable RegisterTable;
+    private JTable DataTable;
+    private JLabel StatisticsLabel;
+
 
     // Total data for the OperandTable
-    static private Object[][] fullData,rawData;
-    static private String[] columnNames;
-    static final int[] columnWidths = new int[]{10, 300};
+    static private Object[][] operandFullData, operandRawData, dataFullData;
+    static private Object[][] registerData= new Object[32][2];
+    static private String[] operandColumnNames,registerColumnNames, dataColumnNames;
+    static private DefaultTableModel operandModel, registerModel, dataModel;
+    static private int[] statisticsInfo = new int[9];
+
+    private static final int[] operandColumnWidths = new int[]{50, 500};
+    private static final int[] registerColumnWidths = new int[]{20, 50};
+    private static final int[] dataColumnWidths = new int[]{50, 500};
+    private static final int[] frameSize = new int[]{700, 300};
+    private static final int[] operandSlice = {0,5};
+
     private void update(DefaultTableModel dataModel) {
-        if(slice[0]>=fullData.length-1){
-            rawData = null;
+        if(operandSlice[0]>= operandFullData.length-1){
+            operandRawData = null;
         }
         else {
-            rawData = Arrays.copyOfRange(fullData, slice[0], slice[1]);
+            operandRawData = Arrays.copyOfRange(operandFullData, operandSlice[0], operandSlice[1]);
         }
-        dataModel.setDataVector(rawData, columnNames);
+        dataModel.setDataVector(operandRawData, operandColumnNames);
         dataModel.fireTableDataChanged();
-        TableUtils.setAllMinColumnSize(OperandTable, columnWidths);
-        TableUtils.setAllPreferredColumnSize(OperandTable, columnWidths);
+        TableUtils.setAllMinColumnSize(OperandTable, operandColumnWidths);
+        TableUtils.setAllPreferredColumnSize(OperandTable, operandColumnWidths);
     }
 
-    DataUI(Object[][] inputTotalData, String[] inputColumnNames) {
-        // Configure the table
-        fullData = inputTotalData;
-        // Column names for the OperandTable
-        rawData = Arrays.copyOfRange(fullData, slice[0], slice[1]);
-        columnNames = inputColumnNames;
-        DefaultTableModel dataModel = new DefaultTableModel(rawData, columnNames);
+    // Initialize the operand Table
+    private void initOperandTable(Object[][] inputTotalData){
+        operandFullData = inputTotalData;
+        operandRawData = Arrays.copyOfRange(operandFullData, operandSlice[0], operandSlice[1]);
+        operandColumnNames = new String[]{"PC","Operand"};
+        operandModel = new DefaultTableModel(operandRawData, operandColumnNames);
 
-        OperandTable.setModel(dataModel);
+        OperandTable.setModel(operandModel);
         OperandTable.setFillsViewportHeight(true);
 
-        TableUtils.setAllMinColumnSize(OperandTable, columnWidths);
-        TableUtils.setAllPreferredColumnSize(OperandTable, columnWidths);
+        TableUtils.setAllMinColumnSize(OperandTable, operandColumnWidths);
+        TableUtils.setAllPreferredColumnSize(OperandTable, operandColumnWidths);
+    }
+
+    // Initialize the register Table
+    private void initRegisterTable(){
+        registerColumnNames = new String[]{"Reg", "Value"};
+        for(int i=0; i<32; i++){
+            System.out.println("R"+ i +"=");
+            registerData[i][0] = "R"+ i +"=";
+            registerData[i][1] = 0;
+        }
+        registerModel = new DefaultTableModel(registerData, registerColumnNames);
+
+        RegisterTable.setModel(registerModel);
+        RegisterTable.setFillsViewportHeight(true);
+
+        TableUtils.setAllMinColumnSize(RegisterTable, registerColumnWidths);
+        TableUtils.setAllPreferredColumnSize(RegisterTable, registerColumnWidths);
+    }
+
+    // Initialize the Data Table
+    private void initDataTable(Object[][] inputData){
+        dataFullData = inputData;
+        dataColumnNames = new String[]{"PC", "Content"};
+        dataModel = new DefaultTableModel(dataFullData, dataColumnNames);
+
+        DataTable.setModel(dataModel);
+        DataTable.setFillsViewportHeight(true);
+
+        TableUtils.setAllMinColumnSize(DataTable, dataColumnWidths);
+        TableUtils.setAllPreferredColumnSize(DataTable, dataColumnWidths);
+    }
+
+    // Initialize Statistics Panel
+    private void initStatisticsPanel(){
+        StatisticsLabel.setText(
+                        "<html><font color='red'>Execution</font><br>" +
+                        statisticsInfo[0] + " Cycles<br>" +
+                        statisticsInfo[1] + " Instructions<br><br>" +
+                        "<font color='red'>Stalls<br></font>" +
+                        statisticsInfo[2] + " RAW Stalls<br>" +
+                        statisticsInfo[3] + " WAW Stalls<br>" +
+                        statisticsInfo[4] + " WAR Stalls<br>" +
+                        statisticsInfo[5] + " Structural Stalls<br>" +
+                        statisticsInfo[6] + " Branch Taken Stalls<br>" +
+                        statisticsInfo[7] + " Branch Mis-prediction Stalls<br><br>" +
+                        "<font color='red'>Code Size</font><br>" +
+                        statisticsInfo[8] + " Bytes"
+                );
+    }
+
+    // Define the menu bar
+    private JMenuBar addMenuBar() {
+        // Start dealing with MenuBar
+        JMenuBar mainMenuBar = new JMenuBar();
+
+        // Menus
+        JMenu mFile = new JMenu("File");
+        JMenu mExec = new JMenu("Execute");
+        JMenu mConf = new JMenu("Configure");
+        JMenu mWind = new JMenu("Window");
+        JMenu mHelp = new JMenu("Help");
+
+        // Add menus to menu bar
+        mainMenuBar.add(mFile);
+        mainMenuBar.add(mExec);
+        mainMenuBar.add(mConf);
+        mainMenuBar.add(mWind);
+        mainMenuBar.add(mHelp);
+
+        // Add menu items to menu mFile
+        String[] fileItemNames = {"Open", "Reset", "Full Reset", "Exit"};
+        Map< String, JMenuItem> fileItems = new HashMap< String, JMenuItem>();
+        for (String itemName:fileItemNames){
+            fileItems.put(itemName, new JMenuItem(itemName));
+            mFile.add(fileItems.get(itemName));
+        }
+
+        // Add listener to Open, here a *.s file is needed, if the file chooser successfully get
+        // a *.s file, it will open DataUI and pass the opened file to it.
+        fileItems.get("Open").addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                FileDialog fd = new FileDialog(new JFrame(), "Choose a file", FileDialog.LOAD);
+                fd.setDirectory("~/Downloads/");
+                fd.setFilenameFilter((dir, name) -> name.endsWith(".s"));
+                fd.setVisible(true);
+                String fileName = fd.getFile();
+                String filePath = fd.getDirectory();
+                if (fileName == null)
+                    System.out.println("You cancelled the choice");
+                else
+                    System.out.println("You chose " + filePath + fileName);
+                try {
+                    String selectedFileName = filePath + fileName;
+                    Map<String, List<Object[]>> listFlagMap = ParseFile.parseFile(new File(selectedFileName));
+                    operandFullData = listFlagMap.get("textList").toArray(new Object[0][0]);
+                    dataFullData = listFlagMap.get("dataList").toArray(new Object[0][0]);
+                    // TODO: Add a global init function
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        fileItems.get("Exit").addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        // Add menu items to menu mExec
+        String[] execItemNames = {"Single Cycle", "Multi Cycles", "Run to", "Stop"};
+        Map< String, JMenuItem> execItems = new HashMap< String, JMenuItem>();
+        for (String itemName:execItemNames){
+            execItems.put(itemName, new JMenuItem(itemName));
+            mExec.add(execItems.get(itemName));
+        }
+
+        // Add menu items to menu mConf
+        // Here may need some other Configures
+        String[] confItemNames = {"Architecture", "Multi-Step"};
+        Map< String, JMenuItem> confItems = new HashMap< String, JMenuItem>();
+        for (String itemName:confItemNames){
+            confItems.put(itemName, new JMenuItem(itemName));
+            mConf.add(confItems.get(itemName));
+        }
+
+        // Add menu items to menu mWind
+        String[] windItemNames = {"Code", "Statistics", "Data", "Registers", "Pipeline", "Cycles", "Terminal"};
+        Map< String, JMenuItem> windItems = new HashMap< String, JMenuItem>();
+        for (String itemName:windItemNames){
+            windItems.put(itemName, new JMenuItem(itemName));
+            mWind.add(windItems.get(itemName));
+        }
+
+        // Add menu items to menu mHelp
+        String[] helpItemNames = {"About Tomasulo Visual"};
+        Map< String, JMenuItem> helpItems = new HashMap< String, JMenuItem>();
+        for (String itemName:helpItemNames){
+            helpItems.put(itemName, new JMenuItem(itemName));
+            mHelp.add(helpItems.get(itemName));
+        }
+
+        return mainMenuBar;
+    }
+
+    public DataUI(Object[][] inputOperandFieldData, Object[][] inputDataFieldData) {
+        // Initialize the operand Table
+        initOperandTable(inputOperandFieldData);
+
+        // Initialize the register Table
+        initRegisterTable();
+
+        // Initialize the data Table
+        initDataTable(inputDataFieldData);
+
+        // Initialize the Statistics Panel
+        initStatisticsPanel();
 
         // Execute one step button action
         ExecuteOneStepBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                slice[0]++;
-                slice[1]++;
-                update(dataModel);
+                operandSlice[0]++;
+                operandSlice[1]++;
+                update(operandModel);
             }
         });
 
@@ -59,16 +236,17 @@ public class DataUI {
         ExecuteMultipleStepBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                slice[0]+=3;
-                slice[1]+=3;
-                update(dataModel);
+                operandSlice[0]+=3;
+                operandSlice[1]+=3;
+                update(operandModel);
             }
         });
 
         JFrame frame = new JFrame("Operands");
         frame.setContentPane(PanelMain);
+        frame.setJMenuBar(addMenuBar());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 180);
+        frame.setSize(frameSize[0], frameSize[1]);
         UICommonUtils.makeFrameToCenter(frame);
         frame.setVisible(true);
     }
--- src/com/miracleyoo/UIs/MainUI.java
+++ src/com/miracleyoo/UIs/MainUI.java
@@ -5,7 +5,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
@@ -16,60 +15,12 @@ public class MainUI {
     private JButton ChooseFileBtn;
     private JPanel PanelMain;
     private JLabel FileSelectedCap;
-    private JLabel FileSelectedVal;
-    private File selectedFile;
+    private String selectedFileName;
     private Map< String, List<Object[]>> listFlagMap;
 
     private MainUI() {
         assert ChooseFileBtn != null;
         ChooseFileBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFileChooser jfc=new JFileChooser();
-                jfc.setFileSelectionMode(JFileChooser.FILES_ONLY );
-                jfc.showDialog(new JLabel(), "Choose");
-                File file=jfc.getSelectedFile();
-                System.out.println("==> File:"+file.getAbsolutePath());
-                System.out.println(jfc.getSelectedFile().getName());
-                try {
-                    listFlagMap = ParseFile.parseFile(file);
-                    Object[][] dataListArray = listFlagMap.get("textList").toArray(new Object[0][0]);
-                    new DataUI(dataListArray, new String[]{"PC","Operand"});
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-            }
-        });
-    }
-
-    private JMenuBar addMenuBar() {
-        // Start dealing with MenuBar
-        JMenuBar mainMenuBar = new JMenuBar();
-
-        // Menus
-        JMenu mFile = new JMenu("File");
-        JMenu mExec = new JMenu("Execute");
-        JMenu mConf = new JMenu("Configure");
-        JMenu mWind = new JMenu("Window");
-        JMenu mHelp = new JMenu("Help");
-
-        // Add menus to menu bar
-        mainMenuBar.add(mFile);
-        mainMenuBar.add(mExec);
-        mainMenuBar.add(mConf);
-        mainMenuBar.add(mWind);
-        mainMenuBar.add(mHelp);
-
-        // Add menu items to menu mFile
-        String[] fileItemNames = {"Open", "Reset", "Full Reset", "Exit"};
-        Map< String, JMenuItem> fileItems = new HashMap< String, JMenuItem>();
-        for (String itemName:fileItemNames){
-            fileItems.put(itemName, new JMenuItem(itemName));
-            mFile.add(fileItems.get(itemName));
-        }
-
-        // Add listener to Open
-        fileItems.get("Open").addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
                 FileDialog fd = new FileDialog(new JFrame(), "Choose a file", FileDialog.LOAD);
@@ -83,56 +34,16 @@ public class MainUI {
                 else
                     System.out.println("You chose " + filePath + fileName);
                 try {
-                    listFlagMap = ParseFile.parseFile(new File(filePath+fileName));
-                    Object[][] dataListArray = listFlagMap.get("textList").toArray(new Object[0][0]);
-                    new DataUI(dataListArray, new String[]{"PC","Operand"});
+                    selectedFileName = filePath+fileName;
+                    listFlagMap = ParseFile.parseFile(new File(selectedFileName));
+                    Object[][] operandListArray = listFlagMap.get("textList").toArray(new Object[0][0]);
+                    Object[][] dataListArray = listFlagMap.get("dataList").toArray(new Object[0][0]);
+                    new DataUI(operandListArray, dataListArray);
                 } catch (IOException ex) {
                     ex.printStackTrace();
                 }
             }
         });
-
-        fileItems.get("Exit").addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
-
-        // Add menu items to menu mExec
-        String[] execItemNames = {"Single Cycle", "Multi Cycles", "Run to", "Stop"};
-        Map< String, JMenuItem> execItems = new HashMap< String, JMenuItem>();
-        for (String itemName:execItemNames){
-            execItems.put(itemName, new JMenuItem(itemName));
-            mExec.add(execItems.get(itemName));
-        }
-
-        // Add menu items to menu mConf
-        // Here may need some other Configures
-        String[] confItemNames = {"Architecture", "Multi-Step"};
-        Map< String, JMenuItem> confItems = new HashMap< String, JMenuItem>();
-        for (String itemName:confItemNames){
-            confItems.put(itemName, new JMenuItem(itemName));
-            mConf.add(confItems.get(itemName));
-        }
-
-        // Add menu items to menu mWind
-        String[] windItemNames = {"Code", "Statistics", "Data", "Registers", "Pipeline", "Cycles", "Terminal"};
-        Map< String, JMenuItem> windItems = new HashMap< String, JMenuItem>();
-        for (String itemName:windItemNames){
-            windItems.put(itemName, new JMenuItem(itemName));
-            mWind.add(windItems.get(itemName));
-        }
-
-        // Add menu items to menu mHelp
-        String[] helpItemNames = {"About Tomasulo Visual"};
-        Map< String, JMenuItem> helpItems = new HashMap< String, JMenuItem>();
-        for (String itemName:helpItemNames){
-            helpItems.put(itemName, new JMenuItem(itemName));
-            mHelp.add(helpItems.get(itemName));
-        }
-
-        return mainMenuBar;
     }
 
     public static void main(String[] args) {
@@ -141,11 +52,10 @@ public class MainUI {
         frame.setContentPane(mainUI.PanelMain);
 
         // Add menu bar to frame
-        frame.setJMenuBar(mainUI.addMenuBar());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 200);
         UICommonUtils.makeFrameToCenter(frame);
-        frame.pack();
+//        frame.pack();
         frame.setVisible(true);
     }
 }
