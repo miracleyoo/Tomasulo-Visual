3352d1915ea6cc0ba2c3e6028eaaa87bb4d2de64
miracleyou <786671043@qq.com>
2019-11-25 21:33:25 -0500

    resolve color problem with rd.

--- src/com/miracleyoo/UIs/Diagram.java
+++ src/com/miracleyoo/UIs/Diagram.java
@@ -23,7 +23,6 @@ public class Diagram extends JPanel {
     static int registers = 10;
     public static int diagramWidth = 600 + 200 + 50 + 50; // Most left: -200; Most right: 200 + 50(rect width)
     public static int diagramHeight = 110 + height * OpQueue + height + 30; // Most down: -110; Most top: Reg rects top
-//    this.setSize(diagramWidth, diagramHeight);
 
     @Override
     public Dimension getPreferredSize() {
@@ -33,6 +32,7 @@ public class Diagram extends JPanel {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        g.setColor(Color.decode(DataUI.colorSchemeMainCur[6]));
 //        setSize(diagramWidth, diagramHeight);
 
         //Allows for window scaling while keeping objects in their relative positions
@@ -48,7 +48,6 @@ public class Diagram extends JPanel {
             g.drawRect(originX +ldBase[0], originY - (height * i + height) + ldBase[1], 50, height);
         }
 
-        g.setColor(Color.BLACK);
 
         //Place sdBuffers
         int[] sdBase = {250, -60};
@@ -56,9 +55,7 @@ public class Diagram extends JPanel {
         for (int i = 0; i < sdBuffer; i++) {
             g.drawRect(originX + sdBase[0], originY - (height * i + height) + sdBase[1], 50, height);
         }
-        //g.setColor((Color.RED));
-        //g.drawLine(originX + sdBase[0] + 25, originY + sdBase[1], originX + sdBase[0] + 25, originY + 100);
-        g.setColor(Color.BLACK);
+//        g.setColor(Color.decode(DataUI.colorSchemeMainCur[6]));
 
         //Place OpQueue -- Note Op Queue is currently implementing both int and fp, THIS MAY NEED TO CHANGE!
         g.drawString("OP Queue", originX - 100, originY - (height * OpQueue + height) - 60);
@@ -75,13 +72,13 @@ public class Diagram extends JPanel {
         //Place integer FU
         int intBase[] = {-300, 60}; //x, y
         for(int a = 0; a < integerRS; a++){
-            g.setColor(Color.BLACK);
+            g.setColor(Color.decode(DataUI.colorSchemeMainCur[6]));
             g.drawRect(originX - opBoxWidth + intBase[0], originY - (height * a) + intBase[1], opBoxWidth, height);
             g.drawRect(originX + intBase[0], originY - (height * a) + intBase[1], operandWidth, height);
             g.drawRect(originX + operandWidth + intBase[0], originY - (height * a) + intBase[1], operandWidth, height);
             g.drawString("IntegerFU", originX + intBase[0] + 5, originY + intBase[1] + 30);
             g.drawRect(originX + intBase[0], originY + intBase[1] + 20, 80, height);
-            g.setColor(Color.RED);
+            g.setColor(Color.decode(DataUI.colorSchemeCycleCur[0]));
             g.drawLine(originX + intBase[0] + 40, originY + intBase[1] + 30, originX + intBase[0] + 40, originY + 100); //to CDB
             g.fillPolygon(new int[] {originX + intBase[0] + 35, originX + intBase[0] + 40, originX + intBase[0] + 45}, new int[] {originY + 95, originY + 100, originY + 95}, 3);
             g.drawLine(originX + intBase[0] + 90, originY + intBase[1] + 10, originX + intBase[0] + 90, originY + 100); //From CDB
@@ -92,13 +89,13 @@ public class Diagram extends JPanel {
         int addBase[] = {-150, 60}; //used to set origin of adder FU on Tomasulo graph. X and then Y.
         //g.drawString("FPadder", originX + addBase[0], originY + addBase[1]);
         for (int x = 0; x < fpAdderRS; x++) {
-            g.setColor(Color.black);
+            g.setColor(Color.decode(DataUI.colorSchemeMainCur[6]));
             g.drawRect(originX - opBoxWidth + addBase[0], originY - (height * x) + addBase[1], opBoxWidth, height);
             g.drawRect(originX + addBase[0], originY - (height * x) + addBase[1], operandWidth, height);
             g.drawRect(originX + operandWidth + addBase[0], originY - (height * x) + addBase[1], operandWidth, height);
             g.drawString("FPadder", originX + addBase[0] + 5, originY + addBase[1] + 30);
             g.drawRect(originX + addBase[0], originY + addBase[1] + 20, 80, height);
-            g.setColor(Color.RED);
+            g.setColor(Color.decode(DataUI.colorSchemeCycleCur[0]));
             g.drawLine(originX + addBase[0] + 40, originY + addBase[1] + 30, originX + addBase[0] + 40, originY + 100); //to CDB
             g.fillPolygon(new int[] {originX + addBase[0] + 35, originX + addBase[0] + 40, originX + addBase[0] + 45}, new int[] {originY + 95, originY + 100, originY + 95}, 3);
             g.drawLine(originX + addBase[0] + 90, originY + addBase[1] + 10, originX + addBase[0] + 90, originY + 100); //from CDB
@@ -109,13 +106,13 @@ public class Diagram extends JPanel {
         int mulBase[] = {0, 60}; //used to set origin of multiplier FU on Tomasulo graph
         //g.drawString("FPmultiplier", originX + mulBase[0], originY + mulBase[1]);
         for (int y = 0; y < fpMultiplierRS; y++) {
-            g.setColor(Color.black);
+            g.setColor(Color.decode(DataUI.colorSchemeMainCur[6]));
             g.drawRect(originX - opBoxWidth + mulBase[0], originY - (height * y) + mulBase[1], opBoxWidth, height);
             g.drawRect(originX + mulBase[0], originY - (height * y) + mulBase[1], operandWidth, height);
             g.drawRect(originX + operandWidth + mulBase[0], originY - (height * y) + mulBase[1], operandWidth, height);
             g.drawString("FPmult", originX + mulBase[0] + 5, originY + mulBase[1] + 30);
             g.drawRect(originX + mulBase[0], originY + mulBase[1] + 20, 80, height);
-            g.setColor(Color.RED);
+            g.setColor(Color.decode(DataUI.colorSchemeCycleCur[0]));
             g.drawLine(originX + mulBase[0] + 40, originY + mulBase[1] + 30, originX + mulBase[0] + 40, originY + 100); //to CDB
             g.fillPolygon(new int[] {originX + mulBase[0] + 35, originX + mulBase[0] + 40, originX + mulBase[0] + 45}, new int[] {originY + 95, originY + 100, originY + 95}, 3);
             g.drawLine(originX + mulBase[0] + 90, originY + mulBase[1] + 10, originX + mulBase[0] + 90, originY + 100); //from CDB
@@ -126,13 +123,13 @@ public class Diagram extends JPanel {
         int divBase[] = {150, 60};
         //g.drawString("Divider", 190, 80);
         for (int z = 0; z < fpDividerRS; z++) {
-            g.setColor(Color.black);
+            g.setColor(Color.decode(DataUI.colorSchemeMainCur[6]));
             g.drawRect(originX - opBoxWidth + divBase[0], originY - (height * z) + divBase[1], opBoxWidth, height);
             g.drawRect(originX + divBase[0], originY - (height * z) + divBase[1], operandWidth, height);
             g.drawRect(originX + operandWidth + divBase[0], originY - (height * z) + divBase[1], operandWidth, height);
             g.drawString("FPdiv", originX + divBase[0] + 5, originY + divBase[1] + 30);
             g.drawRect(originX + divBase[0], originY + divBase[1] + 20, 80, height);
-            g.setColor(Color.RED);
+            g.setColor(Color.decode(DataUI.colorSchemeCycleCur[0]));
             g.drawLine(originX + divBase[0] + 40, originY + divBase[1] + 30, originX + divBase[0] + 40, originY + 100); //to CDB
             g.fillPolygon(new int[] {originX + divBase[0] + 35, originX + divBase[0] + 40, originX + divBase[0] + 45}, new int[] {originY + 95, originY + 100, originY + 95}, 3);
             g.drawLine(originX + divBase[0] + 90, originY + divBase[1] + 10, originX + divBase[0] + 90, originY + 100); //from CDB
@@ -141,9 +138,9 @@ public class Diagram extends JPanel {
         }
 
         //---Connecting Wires---
-        g.setColor((Color.black));
+        g.setColor((Color.decode(DataUI.colorSchemeMainCur[6])));
         g.drawString("Common Data Bus", originX - 400, originY + 110);
-        g.setColor(Color.RED);
+        g.setColor(Color.decode(DataUI.colorSchemeCycleCur[0]));
         g.drawLine(originX + ldBase[0] + 25, originY + ldBase[1], originX + ldBase[0] + 25, originY + 100);
         g.fillPolygon(new int[] {originX + ldBase[0] + 20 , originX + ldBase[0] + 25 , originX + ldBase[0] + 30}, new int[] {originY + 90, originY + 100, originY + 90}, 3);
         g.drawLine(originX + ldBase[0],originY + 100, originX + sdBase[0] + 100, originY + 100); //CBD horizontal line
@@ -154,7 +151,7 @@ public class Diagram extends JPanel {
         g.fillPolygon(new int[] {originX + 50 + 80, originX + 50 + 90, originX + 50 + 90}, new int[] {originY + sdBase[1] - registers*height + 5, originY + sdBase[1] - registers*height + 10, originY + sdBase[1] - registers*height}, 3);
 
         //OpQueue Wires
-        g.setColor(Color.BLUE);
+        g.setColor(Color.decode(DataUI.colorSchemeCycleCur[2]));
         g.drawLine(originX - 100 + 40, originY - 60, originX - 100 + 40, originY - 40); //Vertical line connecting Op-Queue
         g.drawLine(originX - opBoxWidth + intBase[0] + 15, originY + intBase[1] - (integerRS - 1)*height, originX - opBoxWidth + intBase[0] + 15, originY - 40); //Vertical line connecting intOp
         g.fillPolygon(new int[] {originX - opBoxWidth + intBase[0] + 10, originX - opBoxWidth + intBase[0] + 15, originX - opBoxWidth + intBase[0] + 20}, new int[] {originY + intBase[1] - (integerRS - 1)*height - 5, originY + intBase[1] - (integerRS - 1)*height, originY + intBase[1] - (integerRS - 1)*height - 5}, 3);
@@ -167,7 +164,7 @@ public class Diagram extends JPanel {
         g.drawLine(originX - opBoxWidth + intBase[0] + 15, originY - 40, originX - opBoxWidth + divBase[0] + 15, originY - 40); //Horizontal connecting line
 
         //src1 line
-        g.setColor(Color.MAGENTA);
+        g.setColor(Color.decode(DataUI.colorSchemeCycleCur[1]));
         g.drawLine(originX + 50 + 10, originY - 60, originX + 50 + 10, originY - 30); //Vertical line from register connecting to src1
         g.drawLine(originX + intBase[0] + 25, originY + intBase[1] - (integerRS - 1)*height, originX + intBase[0] + 25, originY - 30); //Vertical line for intRS src1
         g.fillPolygon(new int[] {originX + intBase[0] + 20, originX + intBase[0] + 25, originX + intBase[0] + 30}, new int[] {originY + intBase[1] - (integerRS - 1)*height - 5, originY + intBase[1] - (integerRS - 1)*height, originY + intBase[1] - (integerRS - 1)*height - 5}, 3);
@@ -181,7 +178,7 @@ public class Diagram extends JPanel {
 
 
         //src2 line
-        g.setColor(Color.ORANGE);
+        g.setColor(Color.decode(DataUI.colorSchemeCycleCur[3]));
         g.drawLine(originX + 50 + 70, originY - 60, originX + 50 + 70, originY - 50); //Vertical line from register connecting to src2
         g.drawLine(originX + operandWidth + intBase[0] + 25, originY + intBase[1] - (integerRS - 1)*height, originX + operandWidth + intBase[0] + 25, originY - 50); //Vertical line for intRS src2
         g.fillPolygon(new int[] {originX + operandWidth + intBase[0] + 20, originX + operandWidth + intBase[0] + 25, originX + operandWidth + intBase[0] + 30}, new int[] {originY + intBase[1] - (integerRS - 1)*height - 5, originY + intBase[1] - (integerRS - 1)*height, originY + intBase[1] - (integerRS - 1)*height - 5}, 3);
